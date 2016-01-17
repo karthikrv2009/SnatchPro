93178af778058434a80b2f77c8487a96ee9e9f58
karthikrv2009 <karthikrv2009@gmail.com>
2016-01-16 22:46:05 -0500

    Initial commit for Snatch Pro

---
+++ SnatchPRO/src/main/java/org/rvsystem/snatchpro/SnatchProApplication.java
@@ -0,0 +1,12 @@
+package org.rvsystem.snatchpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnatchProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnatchProApplication.class, args);
+	}
+}
---
+++ SnatchPRO/src/test/java/org/rvsystem/snatchpro/SnatchProApplicationTests.java
@@ -0,0 +1,18 @@
+package org.rvsystem.snatchpro;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@WebIntegrationTest
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SnatchProApplication.class)
+public class SnatchProApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
